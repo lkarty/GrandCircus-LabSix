c2144fba90e0a3ba2a821b9561078286cec9ad54
Lindsay Karty <lmkarty@gmail.com>
2018-10-14 15:57:24 -0400

    added special messages

--- src/DiceRoll.java
+++ src/DiceRoll.java
@@ -1,28 +1,41 @@
 import java.util.Random;
 import java.util.Scanner;
 
-
 public class DiceRoll {
-	
+	static int random1, random2, sides;
+	static String choice, choice2;
+
+	private static void rollDice(Random randomizer, Scanner userInput) {
+		do {
+			random1 = randomizer.nextInt(sides) + 1;
+			random2 = randomizer.nextInt(sides) + 1;
+			System.out.println("Result:\n\t" + random1 + "\n\t" + random2);
+			if ((random1 + random2) == 1) {
+				System.out.println("\tSnake eyes");
+			} else if ((random1 + random2) == 7) {
+				System.out.println("\tCraps");
+			} else if ((random1 + random2) == 12) {
+				System.out.println("\tBox cars");
+			}
+			System.out.println("Roll again?(y/n): ");
+			choice = userInput.next();
+			userInput.nextLine();
+		} while (choice.equalsIgnoreCase("y"));
+	}
+
 	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
-		System.out.println("How many sides should each die have?");
-		DiceRoll dice = new DiceRoll(scan.nextInt());
-	    System.out.println("\nThe dice rolled a " + dice.roll() + ".");
-
+		Random randomizer = new Random();
+		Scanner userInput = new Scanner(System.in);
+		System.out.println("Welcome to the Grand Circus Casino!");
+		System.out.println("How many sides should each die have?): ");
+		sides = userInput.nextInt();
+		userInput.nextLine();
+		System.out.println("Roll the dice?(y/n): ");
+		choice = userInput.next();
+		userInput.nextLine();
+		rollDice(randomizer, userInput);
+		System.out.println("Goodbye");
+		userInput.close();
 	}
-   
-	final Random rand;
-	final int dSides;
-	
-	public DiceRoll(int dSides) {
-		this.rand = new Random();
-		this.dSides = dSides;
-	}
-
-	public int roll() {
-		return 1 + rand.nextInt(dSides);
-	}	
-	
 
 }
